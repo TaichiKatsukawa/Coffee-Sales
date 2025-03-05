96975f62b7c3d3cc1a40834f6b9ad4490609ecfc
Taichi Katsukawa <taichi.katsukawa@tendenciaagro.com.br>
2025-03-05 09:51:21 -0400

    "Adding datasets csv to git"

--- coffee-sales.py
+++ coffee-sales.py
@@ -1,15 +1,17 @@
-import kagglehub
+import pprint
+
 
 def main():
-    update_dataset()
+    dataset = get_dataset()
     
 
-def update_dataset():
+def get_dataset():
+    import kagglehub
+
     # Download latest version
     path = kagglehub.dataset_download("ihelon/coffee-sales")
 
     print("Path to dataset files:", path)
-
 
 if __name__ == '__main__':
     main()
